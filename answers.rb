a90b8631034612911357d6965262ad388b50dd3f
Max Alcala <maxarturo@gmail.com>
2015-09-09 19:43:10 -0500

    Add basic xml parsing

--- answers.rb
+++ answers.rb
@@ -11,6 +11,16 @@ def filter_transactions(trans_number)
 end
 
 def load_conversions(conversion_file)
+  doc = REXML::Document.new File.new(conversion_file)
+  conversions = []
+
+  doc.elements.each('*/rate') do |p|
+    curr_conversion = []
+    p.elements.each do |el|
+      curr_conversion.push el.text
+    end
+    conversions.push curr_conversion
+  end
 end
 
-p filter_transactions('DM1182')
+load_conversions('RATES.xml')
